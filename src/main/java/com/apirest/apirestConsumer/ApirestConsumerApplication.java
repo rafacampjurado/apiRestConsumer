cf8d218e38c598ff29c50f8c695684ea6574ca54
rafacampjurado <rcj.es1997@gmail.com>
2025-07-07 16:00:30 +0200

    :tada: first commit

---
+++ src/main/java/com/apirest/apirestConsumer/ApirestConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.apirest.apirestConsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApirestConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApirestConsumerApplication.class, args);
+	}
+
+}
